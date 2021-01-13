b32edb5a493bd8582c0b88591c6217769bf152e3
Jared Matos <Jaredm1121@gmail.com>
2021-01-12 19:37:38 -0500

    typo causing trouble with new window for tooltips

--- noteGenScript.js
+++ noteGenScript.js
@@ -9,14 +9,11 @@ function openNewWindow(){
     var doc = myWindow.document;
     doc.open();
     doc.write('<!DOCTYPE html>\
-        <html lang="en">\
         <head>\
-        <meta charset="UTF-8">\
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">\
         <title>Note Generator</title>\
+        <link rel="preload" href="./tDivStyle.css" as="style">\
+        <link rel="stylesheet" href="./tDivStyle.css" type="text/css">\
         </head>\
-        <link rel="stylesheet" href="tDivStyle.css">\
         <div id="tDiv">');
     doc.write(divText);
     doc.write("</div>\
